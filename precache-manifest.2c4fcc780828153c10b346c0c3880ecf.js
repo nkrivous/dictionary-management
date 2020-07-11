7ca81e13a69257e2a4c96b7ea07f430645ac01a9
nkrivous <kolsln@list.ru>
2020-07-11 20:49:42 +0000

    Deploying to gh-pages from master 4449498410fda749d3b70fbcf75580076c55829d

---
+++ precache-manifest.2c4fcc780828153c10b346c0c3880ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8353b15ac52947517e0747f91533fcb7",
+    "url": "/dictionary-management/index.html"
+  },
+  {
+    "revision": "f4158089b1ee85aafea2",
+    "url": "/dictionary-management/static/css/main.0ef745ae.chunk.css"
+  },
+  {
+    "revision": "eeee5bde01df917b6c1a",
+    "url": "/dictionary-management/static/js/2.ed828632.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/dictionary-management/static/js/2.ed828632.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4158089b1ee85aafea2",
+    "url": "/dictionary-management/static/js/main.c448f281.chunk.js"
+  },
+  {
+    "revision": "9262ccb4bbcc4c3f607a",
+    "url": "/dictionary-management/static/js/runtime-main.182e8d0c.js"
+  }
+]);
